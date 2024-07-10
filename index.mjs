2964df5448248d96da5f847404f1ebe7d168d1e2
Bryan Sharpley <motosharpley@gmail.com>
2024-07-10 10:52:30 -0700

    add a valid transaction to blink

--- index.mjs
+++ index.mjs
@@ -165,14 +165,14 @@ app.get('/actions/vote', (req, res) => {
     label: 'Vote',
     links: {
       actions: [
-        {
-          label: 'Vote MEME1', // button text
-          href: '/actions/memes/vote?choice=meme1',
-        },
-        {
-          label: 'Vote MEME2', // button text
-          href: '/actions/memes/vote?choice=meme2',
-        },
+        // {
+        //   label: 'Vote MEME1', // button text
+        //   href: '/actions/memes/vote?choice=meme1',
+        // },
+        // {
+        //   label: 'Vote MEME2', // button text
+        //   href: '/actions/memes/vote?choice=meme2',
+        // },
         {
           label: 'Send USDC', // button text
           href: '/actions/vote',
@@ -184,20 +184,85 @@ app.get('/actions/vote', (req, res) => {
 
 // Define a Solana Actions POST endpoint
 app.post('/actions/vote', async (req, res) => {
-  // Account provided in the transaction request body by the wallet.
-  const accountField = req.body.account;
+  // Account provided in the transaction req.body by the wallet.
+  const accountField = req.body?.account;
   if (!accountField) throw new Error('missing account');
+  if (typeof accountField !== 'string') throw new Error('invalid account');
   const sender = new PublicKey(accountField);
 
-  // TODO Build a transaction object
+  const splToken = new PublicKey(
+    'Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr'
+  );
+  const MERCHANT_WALLET = new PublicKey(
+    '5CfEty3pffxuWVhRg9VaJjdZ9TBfgHxgPMceEhuikuap'
+  );
 
-  // TODO base64 encoded transaction
-  const base64TX = req.body;
+  async function createSplTransferIx(sender, connection) {
+    // Get the sender's ATA and check that the account exists and can send tokens
+    const senderATA = await getAssociatedTokenAddress(splToken, sender);
+    const senderAccount = await getAccount(connection, senderATA);
+    if (!senderAccount.isInitialized) throw new Error('sender not initialized');
+    if (senderAccount.isFrozen) throw new Error('sender frozen');
+    // console.log('senderAccount', senderAccount);
 
+    const merchantATA = new PublicKey(
+      '6Y9VSMaRYoRLsWeRJiibddfR6CGhnkYvRPnUa2LqTUAS'
+    );
+    // Check that the token provided is an initialized mint
+    const mint = await getMint(connection, splToken);
+    if (!mint.isInitialized) throw new Error('mint not initialized');
+
+    let amount = 10000000;
+    // Check that the sender has enough tokens
+    const tokens = BigInt(String(amount));
+    if (tokens > senderAccount.amount) throw new Error('insufficient funds');
+
+    // Create an instruction to transfer SPL tokens, asserting the mint and decimals match
+    const splTransferIx = createTransferCheckedInstruction(
+      senderATA,
+      splToken,
+      merchantATA,
+      sender,
+      tokens,
+      mint.decimals
+    );
+
+    // Create a reference that is unique to each checkout session
+    const references = [new Keypair().publicKey];
+
+    // add references to the instruction
+    for (const pubkey of references) {
+      splTransferIx.keys.push({ pubkey, isWritable: false, isSigner: false });
+    }
+
+    return splTransferIx;
+  }
+
+  // create spl transfer instruction
+  const splTransferIx = await createSplTransferIx(sender, connection);
+  const blockhash = await connection.getLatestBlockhash();
+  // console.log('splTransferIx', splTransferIx);
+
+  // create the transaction
+  const transactionV0 = new VersionedTransaction(
+    new TransactionMessage({
+      payerKey: sender,
+      recentBlockhash: blockhash.blockhash,
+      // add the instruction to the transaction
+      instructions: [splTransferIx],
+    }).compileToLegacyMessage()
+  );
+
+  const serializedTransaction = transactionV0.serialize();
+
+  const base64Transaction = Buffer.from(serializedTransaction).toString(
+    'base64'
+  );
+  const message = 'Thank you for your purchase!';
   res.set(ACTIONS_CORS_HEADERS);
   res.status(200).json({
-    transaction: base64TX,
-    message: `Hello, ${accountField} Vote POST request!`,
+    transaction: base64Transaction,
+    message: message,
   });
 });
 
